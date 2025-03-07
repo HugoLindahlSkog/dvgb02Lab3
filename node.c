794d4ccbe9a50097ff4a9b388fbd31cecd7f21dd
HugoLindahlSkog <hugoskog03@gmail.com>
2025-03-07 13:45:02 +0100

    .

--- node.c
+++ node.c
@@ -3,22 +3,49 @@
 
 void rtinit(struct distance_table *table, int node) {
 	// Tables are already initialized
+	struct rtpkt pkt;
+	pkt.sourceid = node;
+
     for(int i = 0; i < MAX_NODES; i++){
-		for(int j = 0; j < MAX_NODES; j++){
-			table->costs[i][j] = INF;
+		pkt.mincost[i] = table->costs[i][node];
+	}
+	
+	for(int i = 0; i < MAX_NODES; i++){
+		if(is_neighbor(node, i)){
+			pkt.destid = i;
+			tolayer2(pkt);
 		}
 	}
-
-	table->costs[node][node] = 0;
-
-	/*for(int i = 0; i < MAX_NODES; i++){
-		table->costs[node][i] = tolayer2(pkt);
-	}*/
 
 	printdt(table, node);
 }
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-	printdt(table, node);
+	int source = pkt->sourceid;
+	int updated = 0;
+
+	for(int i = 0; i < MAX_NODES; i++){
+		int new_cost = pkt->mincost[i] + table->costs[source][node];
+		if(new_cost < table->costs[i][node]){
+			table->costs[i][node] = new_cost;
+			updated = 1;
+		}
+	}
+
+	if(updated){
+		struct rtpkt new_pkt;
+		new_pkt.sourceid = node;
+		for(int i = 0; i < MAX_NODES; i++){
+			new_pkt.mincost[i] = table->costs[i][node];
+		}
+		for(int i = 0; i < MAX_NODES; i++){
+			if(is_neighbor(node, i)){
+				new_pkt.destid = 1;
+				tolayer2(new_pkt);
+			}
+		}
+	}
+
+	//printdt(table, node);
 }
 
