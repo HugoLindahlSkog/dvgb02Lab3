447cbe6764e81d30b2857242608e53a3b280dbb6
HugoLindahlSkog <hugoskog03@gmail.com>
2025-03-07 12:09:46 +0100

    node.c

--- node.c
+++ node.c
@@ -1,7 +1,20 @@
 #include "node.h"
+#define MAX_NODES 4
 
 void rtinit(struct distance_table *table, int node) {
 	// Tables are already initialized
+    for(int i = 0; i < MAX_NODES; i++){
+		for(int j = 0; j < MAX_NODES; j++){
+			table->costs[i][j] = INF;
+		}
+	}
+
+	table->costs[node][node] = 0;
+
+	/*for(int i = 0; i < MAX_NODES; i++){
+		table->costs[node][i] = tolayer2(pkt);
+	}*/
+
 	printdt(table, node);
 }
 
