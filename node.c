2b99f94419f160b08be91ac4dc99c8a4c83c9f3f
HugoLindahlSkog <hugoskog03@gmail.com>
2025-03-12 15:49:59 +0100

    börjar fungera

--- node.c
+++ node.c
@@ -2,83 +2,79 @@
 #define MAX_NODES 4
 
 void rtinit(struct distance_table *table, int node) {
-<<<<<<< Updated upstream
-=======
-	/*Fill table with INF with table pointer to distancetable struct, so the table will
-	be filled with 999 meaning we have no clue of distances between nodes yet*/
-	/*for(int i = 0; i < TABLESIZE; i++){
-		for(int j = 0; j < TABLESIZE; j ++){
-			table->costs[i][j] = INF; 
-		}
-	}*/
 
->>>>>>> Stashed changes
-	
 
-	// Tables are already initialized
-	struct rtpkt pkt;
-	pkt.sourceid = node;
 
-    for(int i = 0; i < MAX_NODES; i++){
-		pkt.mincost[i] = table->costs[i][node];
+for(int i = 0; i < MAX_NODES; i++){
+	if(is_neighbor(node, i)){
+		table->costs[i][node] = table->costs[node][i];
+	} else if (i == node) {
+		table->costs[i][node] = 0;
 	}
-<<<<<<< Updated upstream
-=======
-	
-	//for sending routingupdates to neigbours
-	struct rtpkt packet;
-	
-	packet.sourceid = node; //node that sends
->>>>>>> Stashed changes
-	
-	for(int i = 0; i < MAX_NODES; i++){
-		if(is_neighbor(node, i)){
-			pkt.destid = i;
-			tolayer2(pkt);
-		}
-	}
-
-<<<<<<< Updated upstream
-=======
-	for(int i = 0; i < MAX_NODES; i++){
-		if (is_neighbor(node, i)) {
-			table->costs[i][node] = get_link_cost(node, i);
-		}
-	}
-
->>>>>>> Stashed changes
-	printdt(table, node);
 }
 
+
+int mincost[MAX_NODES];
+for(int i = 0; i < MAX_NODES; i++){
+	mincost[i] = table->costs[i][node];
+}
+
+for(int destid = 0; destid < MAX_NODES; destid++){
+	struct rtpkt packet;
+	packet.sourceid = node;
+	packet.destid = destid;
+	for(int i = 0; i < MAX_NODES; i++){
+		packet.mincost[i] = mincost[i];
+	}
+	//printf("Destid: %d, node: %d\n", destid, node);
+
+	if(destid != node && is_neighbor(destid, node)){
+	tolayer2(packet);
+	}
+}
+
+	printdt(table, node);
+}	
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-<<<<<<< Updated upstream
-	int source = pkt->sourceid;
+	
 	int updated = 0;
 
-	for(int i = 0; i < MAX_NODES; i++){
-		int new_cost = pkt->mincost[i] + table->costs[source][node];
-		if(new_cost < table->costs[i][node]){
-			table->costs[i][node] = new_cost;
+	for(int dest = 0; dest < MAX_NODES; dest++){
+		if(dest == node){
+			continue;
+		}
+
+		int cost_to_sender = table->costs[node][pkt->sourceid];
+
+		if(cost_to_sender == INF){
+			continue;
+		}
+
+		int new_cost = pkt->mincost[dest] + cost_to_sender;
+
+		if(new_cost < table->costs[node][dest]){
+			table->costs[node][dest] = new_cost;
+			table->costs[dest][node] = new_cost;
 			updated = 1;
 		}
 	}
 
 	if(updated){
-		struct rtpkt new_pkt;
-		new_pkt.sourceid = node;
-		for(int i = 0; i < MAX_NODES; i++){
-			new_pkt.mincost[i] = table->costs[i][node];
-		}
 		for(int i = 0; i < MAX_NODES; i++){
 			if(is_neighbor(node, i)){
-				new_pkt.destid = 1;
+				struct rtpkt new_pkt;
+				new_pkt.sourceid = node;
+				new_pkt.destid = i;
+
+				for(int j = 0; j < MAX_NODES; j++){
+					new_pkt.mincost[j] = table->costs[node][j];
+					
+				}
 				tolayer2(new_pkt);
 			}
 		}
 	}
-
-=======
->>>>>>> Stashed changes
-	//printdt(table, node);
+	
+	printdt(table, node);
 }
 
