dedeb85acfad9c88638222baf8579a52f357e285
hannescato <ditt@example.com>
2025-03-20 11:59:27 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dvgb02Lab3
+Run program: 
+compile with make
+execute with ./sim
+Enter Trace 0, 1, 2, 
--- node.c
+++ node.c
@@ -2,61 +2,52 @@
 #include <string.h>
 #define MAX_NODES 4
 
-void sendpkt(struct distance_table *table, int node){
-	struct rtpkt packet;
-	packet.sourceid = node;
+void sendpkt(struct distance_table *table, int node) {
+    struct rtpkt packet;
+    packet.sourceid = node;
 
-	for(int neighbor = 0; neighbor < MAX_NODES; neighbor++){
-		if(!is_neighbor(node, neighbor)) continue;
+    for (int neighbor = 0; neighbor < MAX_NODES; neighbor++) {
+        if (!is_neighbor(node, neighbor)) continue;
 
-		packet.destid = neighbor;
+        packet.destid = neighbor;
 
-		for(int i = 0; i < MAX_NODES; i++){
-			packet.mincost[i] = table->costs[node][i];
-		}
-		tolayer2(packet);
-	}
+        int *node_costs = table->costs[node];
+        for (int i = 0; i < MAX_NODES; i++) {
+            packet.mincost[i] = node_costs[i];
+        }
+
+        tolayer2(packet);
+    }
 }
-
 
 void rtinit(struct distance_table *table, int node) {
+    sendpkt(table, node);
+    printdt(table, node);
+}    
 
-	sendpkt(table, node);
-	printdt(table, node);
-}	
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
+    int updated = 0;
 
-	int updated = 0;
-	int new_cost[MAX_NODES][MAX_NODES];
+    int *node_costs = table->costs[node];
 
-	memcpy(new_cost, table->costs, sizeof(new_cost));
+    for (int dest = 0; dest < MAX_NODES; dest++) {
+        int current_cost = node_costs[dest];
+        int cost_via_source = table->costs[node][pkt->sourceid] + pkt->mincost[dest];
 
-	for(int dest = 0; dest < MAX_NODES; dest++){
-		//if(dest == node) continue;
+        table->costs[pkt->sourceid][dest] = pkt->mincost[dest];
 
-		
-		
-		int current_cost = table->costs[node][dest];
-		int cost_via_source = table->costs[node][pkt->sourceid] + pkt->mincost[dest];
+        if (cost_via_source < current_cost) {
+            node_costs[dest] = cost_via_source;
+            updated = 1;
+        }
+    }
 
-		table->costs[pkt->sourceid][dest] = pkt->mincost[dest];
+    if (updated) {
+        sendpkt(table, node);
+    }
 
-		if(cost_via_source < current_cost){
-			new_cost[node][dest] = cost_via_source;
-			updated = 1;
-		}
-
-		
-		
-	}
-
-	if(updated){
-		memcpy(table->costs, new_cost, sizeof(new_cost));
-		sendpkt(table, node);
-	}
-		printdt(table, node);
+    printdt(table, node);
 }
 
-	
 
 
